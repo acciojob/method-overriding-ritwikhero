50fb50fede0ada0b012599543634d44463ecc5cd
Ritwik <ritwikhero@gmail.com>
2023-05-11 17:07:11 +0530

    assingment complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+
+//        String res = obj.meth();
+//        System.out.println(res);
+
+        String res1 = obj.meth();
+        System.out.println(res1);
+    }
+
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
 }
